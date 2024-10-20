4256369005f7ffecda72e24b8119226d681e84d2
VivekCodesCode <152467394+VivekCodesCode@users.noreply.github.com>
2024-10-20 13:06:44 +0530

    animations

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,46 +1,122 @@
 import React from "react";
-import '../CSS/Skills.css'
-import ricon from "../Images/reacticon.png"
-import njs from "../Images/njs.png"
-import java from "../Images/java.png"
-import git from "../Images/git.png"
-import cplus from "../Images/cplus.png"
-import  vscode from "../Images/vscode.png"
+import '../CSS/Skills.css';
+import ricon from "../Images/reacticon.png";
+import njs from "../Images/njs.png";
+import java from "../Images/java.png";
+import git from "../Images/git.png";
+import cplus from "../Images/cplus.png";
+import vscode from "../Images/vscode.png";
 import { motion } from "framer-motion";
-function Skills(params) {
-    return(
+
+function Skills() {
+    return (
         <>
-          <motion.div
-           initial={{ scale: 0 }}
-           animate={{ rotate: 360, scale: 1 }}
-           transition={{
-             type: "spring",
-             stiffness: 260,
-             damping: 20
-           }}
-          className="skills_container">
-            <motion.div className="skill_child">
-                <img  src={ricon}/>
-               <center> <b>React JS</b></center>
+          <motion.div         
+            
+            viewport={{ once: true }} 
+            className="skills_container"
+          >
+            <motion.div className="skill_child"
+            initial={{z:200,x:-100, scale: 0, opacity: 0 }}
+            whileInView={{x:0,z:0, opacity: 1, scale: 1, }}
+            transition={{
+              type: "tween",
+              stiffness: 260,
+              damping: 20,
+              duration:1, 
+               ease: "easeOut"
+            }}
+whileTap={{ scale: 0.2 }}
+            
+            
+            >
+                
+                <img src={ricon} alt="React JS" />
+               <center><b>React JS</b></center>
             </motion.div>
-            <motion.div className="skill_child">
-                <img  src={njs}/>
-                <center> <b>Node JS</b></center>
-            </motion.div><motion.div className="skill_child skill_child3">
-                <img  src={java}/>
-                <center> <b>Java</b></center>
-            </motion.div><motion.div className="skill_child">
-                <img  src={vscode}/>
-                <center> <b>VS Code</b></center>
-            </motion.div><motion.div className="skill_child">
-                <img  src={git}/>
-                <center> <b>Git Hub</b></center>
-            </motion.div><motion.div className="skill_child">
-                <img  src={cplus}/>
-                <center> <b>C++ Programming</b></center>
+
+            <motion.div className="skill_child"
+            initial={{z:200,x:-100, scale: 0, opacity: 0 }}
+            whileInView={{x:0,z:0, opacity: 1, scale: 1, }}
+            transition={{
+              type: "tween",
+              stiffness: 260,
+              damping: 20,
+              duration:1, 
+               ease: "easeOut"
+            }}
+            whileTap={{ scale: 0.2 }}
+            >
+                <img src={njs} alt="Node JS" />
+                <center><b>Node JS</b></center>
+            </motion.div>
+
+            <motion.div className="skill_child"
+            initial={{z:200,x:-100, scale: 0, opacity: 0 }}
+            whileInView={{x:0,z:0, opacity: 1, scale: 1, }}
+            transition={{
+              type: "tween",
+              stiffness: 260,
+              damping: 20,
+              duration:1, 
+               ease: "easeOut"
+            }}
+            whileTap={{ scale: 0.2 }}
+            >
+                <img src={java} alt="Java" />
+                <center><b>Java</b></center>
+            </motion.div>
+
+            <motion.div className="skill_child"
+            initial={{z:200,x:-100, scale: 0, opacity: 0 }}
+            whileInView={{x:0,z:0, opacity: 1, scale: 1, }}
+            transition={{
+              type: "tween",
+              stiffness: 260,
+              damping: 20,
+              duration:1, 
+               ease: "easeOut"
+            }}
+            whileTap={{ scale: 0.2 }}
+            >
+                <img src={vscode} alt="VS Code" />
+                <center><b>VS Code</b></center>
+            </motion.div>
+
+            <motion.div className="skill_child"
+            initial={{z:200,x:-100, scale: 0, opacity: 0 }}
+            whileInView={{x:0,z:0, opacity: 1, scale: 1, }}
+            transition={{
+              type: "tween",
+              stiffness: 260,
+              damping: 20,
+              duration:1, 
+               ease: "easeOut"
+            }}
+            whileTap={{ scale: 0.2 }}
+            >
+                <img src={git} alt="Git" />
+                <center><b>Git Hub</b></center>
+            </motion.div>
+
+            <motion.div className="skill_child"
+            initial={{z:200,x:-100, scale: 0, opacity: 0 }}
+            whileInView={{x:0,z:0, opacity: 1, scale: 1, }}
+            transition={{
+              type: "tween",
+              stiffness: 260,
+              damping: 20,
+              duration:1, 
+               ease: "easeOut"
+            }}
+            whileTap={{ scale: 0.2 }}
+            >
+                <img src={cplus} alt="C++ Programming" />
+                <center><b>C++ Programming</b></center>
             </motion.div>
           </motion.div>
         </>
-    )
+    );
 }
+
 export default Skills;
